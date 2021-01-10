bd32dcd62d231e030c7388e321f97c6f3ad036bf
Christian <christianalva96@gmail.com>
2021-01-10 11:56:20 -0600

    New Repo

---
+++ actions/actions.py
@@ -0,0 +1,57 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionSlotReset(Action):
+
+    def name(self) -> Text:
+        return "action_slot_reset"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [AllSlotsReset()]
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "policy_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["city", "dbo", "appointment"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+
+class actionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_get_dbo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
